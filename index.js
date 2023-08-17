74b44760e10c7097eb195926c9a13e39b5ddd52d
dillon zett <dillonmzett@gmail.com>
2023-08-17 15:49:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element = document.getElementById("main");
+
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+const node = document.createTextNode("dillon is the champion");
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.appendChild(node);
